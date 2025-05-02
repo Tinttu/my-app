f753873ab26fee40b666c065b71fc04b368f525a
Tinttu <tiina.soramies@gmail.com>
2025-05-02 20:42:32 +0300

    navigointi kuntoon

--- src/assets/Endpage.jsx
+++ src/assets/Endpage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 
 const Endpage = () => {
+  const navigate = useNavigate();
   return (
     <div className="endpage-container">
        <h1>Olet läpäissyt harjoituksen!</h1>
-       <p>Onnittelut upeasta saavutuksesta! 🎉</p>
+       <button onClick={() => navigate("/")} className="takaisin-nappi">
+        Takaisin alkuun
+      </button>
+      <p></p>
       <img src= "/onnittelu.jfif" alt="Onnittelukuva" className="endpage-image"  style={{ width: "100%", height: "auto", maxHeight: "65vh", objectFit: "cover"}}/>
      
     </div>
